9c034a253cb50fb7c71a865ba30f73f99abb3418
Adri-Charbonneau <105881448+Adri-Charbonneau@users.noreply.github.com>
2022-11-05 16:26:27 +0100

    Ajout de mastodon

--- end_push.ps1
+++ end_push.ps1
@@ -2,7 +2,7 @@
 git config --local user.email "bi.alive@outlook.fr"
 git config --local user.name "JACK"
 git add .
-git commit -m "[Bot] Update $name"
+git commit -m "[Bot] Mise à jour $name"
 git push -f
 
 # correct characters
@@ -12,10 +12,10 @@ $title = $title -replace '&#233;','é'
 ## length of tweet
 if ( $title.Length -ge 110 )
 { 
-$titletweet = $title.Substring(0, 110)
-$titletweet = -join($titletweet,"...")
-}else{
-  $titletweet = $title
+	$titletweet = $title.Substring(0, 110)
+	$titletweet = -join($titletweet,"...")
+	}else{
+	$titletweet = $title
 }
 
 ## replace character
@@ -28,33 +28,46 @@ $tmtitle = $tmtitle -replace '>','&gt;'
 $tmlink = $link
 $tmlink = $tmlink -replace '&','%26'
 
-## post tweet
+## post twitter tweet
 $twitter = (Select-String -Path "config.txt" -Pattern "twitter=(.*)").Matches.Groups[1].Value
 if ( $twitter -eq "y" )
 {
-Install-Module PSTwitterAPI -Force
-Import-Module PSTwitterAPI
-$OAuthSettings = @{
-ApiKey = "$env:PST_KEY"
-ApiSecret = "$env:PST_KEY_SECRET"
-AccessToken = "$env:PST_TOKEN"
-AccessTokenSecret = "$env:PST_TOKEN_SECRET"
-}
-Set-TwitterOAuthSettings @OAuthSettings
-Send-TwitterStatuses_Update -status "Nouvel article de $name ! $titletweet
-
-Lien : $link
-$accounts
-$tags
-"
+	Install-Module PSTwitterAPI -Force
+	Import-Module PSTwitterAPI
+	$OAuthSettings = @{
+		ApiKey = "$env:PST_KEY"
+		ApiSecret = "$env:PST_KEY_SECRET"
+		AccessToken = "$env:PST_TOKEN"
+		AccessTokenSecret = "$env:PST_TOKEN_SECRET"
+	}
+	Set-TwitterOAuthSettings @OAuthSettings
+	Send-TwitterStatuses_Update -status "Nouvel article de $name ! $titletweet
+	
+	Lien : $link
+	$accounts
+	$tags
+	"
 }
 
 # send telegram notification
 Function Send-Telegram {
-Param([Parameter(Mandatory=$true)][String]$Message)
-$Telegramtoken = "$env:TELEGRAM"
-$Telegramchatid = "$env:CHAT_ID"
-[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+	Param([Parameter(Mandatory=$true)][String]$Message)
+	$Telegramtoken = "$env:TELEGRAM"
+	$Telegramchatid = "$env:CHAT_ID"
+	[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 $Response = Invoke-RestMethod -Uri "https://api.telegram.org/bot$($Telegramtoken)/sendMessage?chat_id=$($Telegramchatid)&text=$($Message)"}
 
 Send-Telegram -Message "Nouvel article de $name : $tmtitle - $tmlink"
+
+# post mastodon toot
+$Uri = 'https://piaille.fr/api/v1/statuses'
+$headers = @{
+	Authorization = "Bearer $env:MASTODON"
+}
+$form = @{
+	status = "Nouvel article de $name ! $titletweet
+	
+	Lien : $link
+	$tags"
+}
+Invoke-WebRequest -Uri $Uri -Headers $headers -Method Post -Form $form
