2951b266101aa7c25558d91ea9cea07380995cd5
Adri-Charbonneau <105881448+Adri-Charbonneau@users.noreply.github.com>
2022-11-28 11:25:28 +0100

    Correction de l'ordre des actualités

--- reseau_cen.ps1
+++ reseau_cen.ps1
@@ -6,10 +6,10 @@ $tags ="#cen #news #biodiversity #science"
 
 # compare two title
 [xml]$old_title = Get-Content ./$id/$id.xml -Encoding UTF8
-$old = $old_title.rss.channel.item.title[1]
+$old = $old_title.rss.channel.item.title[0]
 
 [xml]$new_title = (invoke-webrequest -Uri "https://reseau-cen.org/rss").Content
-$new = $new_title.rss.channel.item.title[1]
+$new = $new_title.rss.channel.item.title[0]
 
 if ( $new -eq $old ) {
 	echo "Le dernier article de $name est déjà existant dans la base de donnée"
@@ -17,8 +17,8 @@ if ( $new -eq $old ) {
 	Invoke-WebRequest -Uri "https://reseau-cen.org/rss" -OutFile "./$id/$id.xml"
 	[xml]$data = Get-Content ./$id/$id.xml -Encoding UTF8
 	
-	$title = $data.rss.channel.item.title[1]
-	$link = $data.rss.channel.item.link[1]
+	$title = $data.rss.channel.item.title[0]
+	$link = $data.rss.channel.item.link[0]
 	
 	./end_push.ps1
 }
