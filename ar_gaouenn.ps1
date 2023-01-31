caa9e49fa16ea7e0654d3b7e179ecd7cc5a0c21e
Adri-Charbonneau <105881448+Adri-Charbonneau@users.noreply.github.com>
2023-01-31 10:39:41 +0100

    Ajout Ar Gaouenn

---
+++ ar_gaouenn.ps1
@@ -0,0 +1,30 @@
+# variables
+$id = "ar_gaouenn"
+$name = "Ar Gaouenn"
+$accounts = ""
+$tags ="#faune #news #biodiversity #science #nature"
+
+# compare two title
+$old = (Select-String -Path "./$id/$id.txt" -Pattern "(.*)").Matches.Groups[1].Value
+
+Invoke-WebRequest -Uri "https://www.lpo.fr/lpo-locales/lpo-bretagne/mediatheque/ar-gaouenn" -OutFile "./$id/$id.html"
+  
+$Source -match '(Ar Gaouenn .*?)<'
+$new = $matches[1]
+$new = $new -replace 'Â°','°'
+
+$Source = Get-Content -path "./$id/$id.html" -raw
+$Source -match '(https://www.lpo.fr/content/download/.*?)"'
+$last = $matches[1]
+
+Remove-Item "./$id/$id.html"
+
+if ( $new -eq $old ) {
+echo "Le dernier article de $name est déjà existant dans la base de donnée"
+} else {
+    $new | Out-File "./$id/$id.txt"
+    
+    $title = $new
+    $link = $last
+    ./end_push.ps1
+}
