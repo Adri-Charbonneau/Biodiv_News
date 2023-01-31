13c7cc2d151588454c4e38052f4b139065d2b7b7
Adri-Charbonneau <105881448+Adri-Charbonneau@users.noreply.github.com>
2023-01-31 10:46:13 +0100

    Correction Ar Gaouenn

--- ar_gaouenn.ps1
+++ ar_gaouenn.ps1
@@ -8,13 +8,13 @@ $tags ="#faune #news #biodiversity #science #nature"
 $old = (Select-String -Path "./$id/$id.txt" -Pattern "(.*)").Matches.Groups[1].Value
 
 Invoke-WebRequest -Uri "https://www.lpo.fr/lpo-locales/lpo-bretagne/mediatheque/ar-gaouenn" -OutFile "./$id/$id.html"
-  
+
+$Source = Get-Content -path "./$id/$id.html" -raw
 $Source -match '(Ar Gaouenn .*?)<'
 $new = $matches[1]
 $new = $new -replace 'Â°','°'
 
-$Source = Get-Content -path "./$id/$id.html" -raw
-$Source -match '(https://www.lpo.fr/content/download/.*?)"'
+$Source -match '(/content/download/.*?)"'
 $last = $matches[1]
 
 Remove-Item "./$id/$id.html"
@@ -25,6 +25,6 @@ echo "Le dernier article de $name est déjà existant dans la base de donnée"
     $new | Out-File "./$id/$id.txt"
     
     $title = $new
-    $link = $last
+    $link = "https://www.lpo.fr/$last"
     ./end_push.ps1
 }
