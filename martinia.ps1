994d33d2e410a1ff2a0dc9ff8b45eb0d72a0941a
Adrien CHARBONNEAU <105881448+Adri-Charbonneau@users.noreply.github.com>
2024-01-19 11:01:09 +0100

    Correction Martinia

--- martinia.ps1
+++ martinia.ps1
@@ -8,13 +8,13 @@ $tags ="#martinia #biodiversité #libellules #odonatologie #entomologie"
 [xml]$old_title = Get-Content ./$id/$id.xml -Encoding UTF8
 $old = $old_title.rss.channel.item.title[0]
 
-[xml]$new_title = (invoke-webrequest -Uri "https://www.martinia.insectes.org/feed/").Content
+[xml]$new_title = (invoke-webrequest -Uri "https://martinia.insectes.org/feed/").Content
 $new = $new_title.rss.channel.item.title[0]
 
 if ( $new -eq $old ) {
 echo "Le dernier article de $name est déjà existant dans la base de donnée"
 } else {
-Invoke-WebRequest -Uri "https://www.martinia.insectes.org/feed/" -OutFile "./$id/$id.xml"
+Invoke-WebRequest -Uri "https://martinia.insectes.org/feed/" -OutFile "./$id/$id.xml"
 [xml]$data = Get-Content ./$id/$id.xml -Encoding UTF8
 
 $title = $data.rss.channel.item.title[0]
